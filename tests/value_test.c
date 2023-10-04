97c31596ec0a424fb27bba5eaa1876282fa64140
fabiosvm <fabiosvm@outlook.com>
2023-10-04 14:17:22 -0300

    update value_test.c

--- tests/value_test.c
+++ tests/value_test.c
@@ -12,19 +12,11 @@ static inline void value_retain_release_test(void);
 
 static inline void value_type_name_test(void)
 {
-  KodValue val;
-  val = KOD_NULL_VALUE;
-  assert(!strcmp(kod_type_name(kod_type(val)), "null"));
-  val = KOD_FALSE_VALUE;
-  assert(!strcmp(kod_type_name(kod_type(val)), "boolean"));
-  val = KOD_TRUE_VALUE;
-  assert(!strcmp(kod_type_name(kod_type(val)), "boolean"));
-  val = kod_number_value(0);
-  assert(!strcmp(kod_type_name(kod_type(val)), "number"));
-  val = kod_rune_value(0);
-  assert(!strcmp(kod_type_name(kod_type(val)), "rune"));
-  val = kod_string_value(NULL);
-  assert(!strcmp(kod_type_name(kod_type(val)), "string"));
+  assert(!strcmp(kod_type_name(KOD_TYPE_NULL), "null"));
+  assert(!strcmp(kod_type_name(KOD_TYPE_BOOLEAN), "boolean"));
+  assert(!strcmp(kod_type_name(KOD_TYPE_NUMBER), "number"));
+  assert(!strcmp(kod_type_name(KOD_TYPE_RUNE), "rune"));
+  assert(!strcmp(kod_type_name(KOD_TYPE_STRING), "string"));
 }
 
 static inline void value_retain_release_test(void)
