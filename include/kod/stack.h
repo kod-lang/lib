55f65ea517fddab2615a12c57caf9548fffe6094
fabiosvm <fabiosvm@outlook.com>
2023-10-26 15:56:42 -0300

    fix stack

--- include/kod/stack.h
+++ include/kod/stack.h
@@ -5,7 +5,6 @@
 #ifndef KOD_STACK_H
 #define KOD_STACK_H
 
-//#include "value.h"
 #include "memory.h"
 
 #define KOD_STACK_DEFAULT_SIZE (1 << 10)
@@ -13,66 +12,46 @@
 #define KodStack(T) \
   struct { \
     int size; \
-    T *slots; \
+    T *elements; \
     T *top; \
     T *end; \
   }
 
 #define kod_stack_init(stk, sz, m, s) \
   do { \
-    KodValue *slots = kod_memory_alloc((m), sizeof(*slots) * (sz), (s)); \
+    size_t size = sizeof(*(stk)->elements) * (sz); \
+    void *elements = kod_memory_alloc((m), size, (s)); \
     if (!(s)->isOk) \
       break; \
     (stk)->size = (sz); \
-    (stk)->slots = slots; \
-    (stk)->top = &slots[-1]; \
-    (stk)->end = &slots[(sz) - 1]; \
+    (stk)->elements = elements; \
+    (stk)->top = &(stk)->elements[-1]; \
+    (stk)->end = &(stk)->elements[(sz) - 1]; \
   } while (0)
 
 #define kod_stack_deinit(stk, m) \
   do { \
-    while ((stk)->top >= (stk)->slots) { \
-      kod_value_release((stk)->top[0], (m)); \
-      --(stk)->top; \
-    } \
-    kod_memory_dealloc((m), (stk)->slots); \
+    kod_memory_dealloc((m), (stk)->elements); \
   } while (0)
 
-#define kod_stack_is_empty(s) ((s)->top == &(s)->slots[-1])
+#define kod_stack_is_empty(s) ((s)->top == &(s)->elements[-1])
 
 #define kod_stack_is_full(s) ((s)->top == (s)->end)
 
 #define kod_stack_peek(s, i) ((s)->top[-(i)])
 
-#define kod_stack_inplace_push(stk, v, s) \
-  do { \
-    kod_stack_inplace_push_value((stk), (v), (s)); \
-    if (!(s)->isOk) \
-      break; \
-    kod_value_retain((v)); \
-  } while (0)
-
-#define kod_stack_inplace_push_value(stk, v, s) \
+#define kod_stack_inplace_push(stk, e, s) \
   do { \
     if ((stk)->top == (stk)->end) { \
       kod_status_error((s), "stack overflow"); \
       break; \
     } \
     ++(stk)->top; \
-    (stk)->top[0] = (v); \
-  } while (0)
-
-#define kod_stack_inplace_push_object(stk, v, s) \
-  do { \
-    kod_stack_inplace_push_value((stk), (v), (s)); \
-    if (!(s)->isOk) \
-      break; \
-    kod_inc_ref(kod_as_object((v))); \
+    (stk)->top[0] = (e); \
   } while (0)
 
 #define kod_stack_inplace_pop(stk, m) \
   do { \
-    kod_value_release((stk)->top[0], (m)); \
     --(stk)->top; \
   } while (0)
 
--- tests/stack_test.c
+++ tests/stack_test.c
@@ -20,8 +20,6 @@ static KodMemory mem = {
 
 static inline void stack_init_test(void);
 static inline void stack_inplace_push_test(void);
-static inline void stack_inplace_push_value_test(void);
-static inline void stack_inplace_push_object_test(void);
 static inline void stack_inplace_pop_test(void);
 
 static void *memory_alloc(size_t size, void *udata)
@@ -46,13 +44,13 @@ static inline void stack_init_test(void)
 {
   KodStatus status;
   kod_status_ok(&status);
-  KodStack(KodValue) stack = { 0 };
+  KodStack(int) stack = { 0 };
   kod_stack_init(&stack, KOD_STACK_DEFAULT_SIZE, &mem, &status);
   assert(status.isOk);
   assert(stack.size == KOD_STACK_DEFAULT_SIZE);
-  assert(stack.slots);
-  assert(stack.top == &stack.slots[-1]);
-  assert(stack.end == &stack.slots[KOD_STACK_DEFAULT_SIZE - 1]);
+  assert(stack.elements);
+  assert(stack.top == &stack.elements[-1]);
+  assert(stack.end == &stack.elements[KOD_STACK_DEFAULT_SIZE - 1]);
   kod_stack_deinit(&stack, &mem);
 }
 
@@ -60,51 +58,13 @@ static inline void stack_inplace_push_test(void)
 {
   KodStatus status;
   kod_status_ok(&status);
-  KodStack(KodValue) stack = { 0 };
+  KodStack(int) stack = { 0 };
   kod_stack_init(&stack, KOD_STACK_DEFAULT_SIZE, &mem, &status);
   assert(status.isOk);
-  kod_stack_inplace_push(&stack, kod_number_value(1.23), &status);
+  kod_stack_inplace_push(&stack, 1, &status);
   assert(status.isOk);
-  assert(stack.top == &stack.slots[0]);
-  assert(kod_as_number(kod_stack_peek(&stack, 0)) == (double) 1.23);
-  KodString *str = kod_string_new_from("foo", &mem, &status);
-  assert(status.isOk);
-  kod_stack_inplace_push(&stack, kod_string_value(str), &status);
-  assert(status.isOk);
-  assert(stack.top == &stack.slots[1]);
-  assert(kod_as_string(kod_stack_peek(&stack, 0)) == str);
-  assert(kod_ref_count(&str->obj) == 1);
-  kod_stack_deinit(&stack, &mem);
-}
-
-static inline void stack_inplace_push_value_test(void)
-{
-  KodStatus status;
-  kod_status_ok(&status);
-  KodStack(KodValue) stack = { 0 };
-  kod_stack_init(&stack, KOD_STACK_DEFAULT_SIZE, &mem, &status);
-  assert(status.isOk);
-  kod_stack_inplace_push_value(&stack, kod_number_value(1.23), &status);
-  assert(status.isOk);
-  assert(stack.top == &stack.slots[0]);
-  assert(kod_as_number(kod_stack_peek(&stack, 0)) == (double) 1.23);
-  kod_stack_deinit(&stack, &mem);
-}
-
-static inline void stack_inplace_push_object_test(void)
-{
-  KodStatus status;
-  kod_status_ok(&status);
-  KodStack(KodValue) stack = { 0 };
-  kod_stack_init(&stack, KOD_STACK_DEFAULT_SIZE, &mem, &status);
-  assert(status.isOk);
-  KodString *str = kod_string_new_from("foo", &mem, &status);
-  assert(status.isOk);
-  kod_stack_inplace_push_object(&stack, kod_string_value(str), &status);
-  assert(status.isOk);
-  assert(stack.top == &stack.slots[0]);
-  assert(kod_as_string(kod_stack_peek(&stack, 0)) == str);
-  assert(kod_ref_count(&str->obj) == 1);
+  assert(stack.top == &stack.elements[0]);
+  assert(kod_stack_peek(&stack, 0) == 1);
   kod_stack_deinit(&stack, &mem);
 }
 
@@ -112,15 +72,13 @@ static inline void stack_inplace_pop_test(void)
 {
   KodStatus status;
   kod_status_ok(&status);
-  KodStack(KodValue) stack = { 0 };
+  KodStack(int) stack = { 0 };
   kod_stack_init(&stack, KOD_STACK_DEFAULT_SIZE, &mem, &status);
   assert(status.isOk);
-  KodString *str = kod_string_new_from("foo", &mem, &status);
-  assert(status.isOk);
-  kod_stack_inplace_push_object(&stack, kod_string_value(str), &status);
+  kod_stack_inplace_push(&stack, 1, &status);
   assert(status.isOk);
   kod_stack_inplace_pop(&stack, &mem);
-  assert(stack.top == &stack.slots[-1]);
+  assert(stack.top == &stack.elements[-1]);
   kod_stack_deinit(&stack, &mem);
 }
 
@@ -128,8 +86,6 @@ int main(void)
 {
   stack_init_test();
   stack_inplace_push_test();
-  stack_inplace_push_value_test();
-  stack_inplace_push_object_test();
   stack_inplace_pop_test();
   return EXIT_SUCCESS;
 }
